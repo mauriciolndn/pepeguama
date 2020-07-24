7c6c5da2d439d793bbaeab97792b77bc36186b03
megane <megane.londono@hotmail.com>
2020-07-24 12:29:50 +0200

    language option on index page fully working, not on others

---
+++ modules/lang.js
@@ -0,0 +1,45 @@
+//export language translations
+export let languages = {
+  english: {
+    profileTitle: "The person behind the art",
+    profileDesc:
+      "Hi, my name is Mauricio Londoño and I am an independent artist who focuses mainly on working with wood, as well as glass. Throughout the years I have been developing works of my own and would now like to share these pieces with you.",
+    home: "home",
+    wood: "wood",
+    glass: "glass",
+    woodSub: "Wood sculptures",
+    woodDesc: "Description about wood work etc.",
+    woodBtn: "Read More",
+    glassSub: "Glass sculptures",
+    glassDesc: "Description about glass work etc.",
+    glassBtn: "Read More",
+  },
+  español: {
+    profileTitle: "La cara detrás del arte",
+    profileDesc:
+      "Hola, mi nombre es Mauricio Londoño y soy un artista independiente que se especialisa en arte en madera y en vidrio. A lo largo de los años, he ido desarrollado piezas individuales que me gustaría compartir con ustedes.",
+    home: "home",
+    wood: "madera",
+    glass: "vidrio",
+    woodSub: "Esculturas en madera",
+    woodDesc: "Descripción sobre arte en madera etc.",
+    woodBtn: "Leer Más",
+    glassSub: "Esculturas en vidrio",
+    glassDesc: "Descripción sobre arte en vidrio etc.",
+    glassBtn: "Leer Más",
+  },
+  français: {
+    profileTitle: "Le visage derrière de l'art",
+    profileDesc:
+      "Bonjour, je m'appelle Mauricio Londoño et je suis un artiste indépendant qui se spécialise sur tout sûr l'art en bois, ainsi que le verre. Au cours des années, j’ai construit pas mal de pièces personnelles que je voudrais mettre en vente pour les partager avec vous.",
+    home: "acceuil",
+    wood: "bois",
+    glass: "verre",
+    woodSub: "Sculptures en bois",
+    woodDesc: "Description sur le travail en bois etc.",
+    woodBtn: "Lire plus",
+    glassSub: "Sculptures en verre",
+    glassDesc: "Description sur le travail en verre etc.",
+    glassBtn: "Lire plus",
+  },
+};
